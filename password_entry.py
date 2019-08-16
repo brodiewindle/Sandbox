061073071e8deecf934527b535ad183a22ba95ad
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2019-08-16 16:17:56 +1000

    Add password check

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,14 @@
 """
 Brodie Windle
 """
+
+
+MIN_LENGTH = 2
+MAX_LENGTH = 15
+
+password = input("Please enter a Password: ")
+
+while MIN_LENGTH > len(password) < MAX_LENGTH:
+    password = input("Please enter a Password: ")
+
+print(len(password)*"*")
